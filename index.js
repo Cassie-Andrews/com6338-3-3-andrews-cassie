f531765fd797d92701c78f4556d36cc7dbfc19b9
Cassie Andrews <candrews1@ufl.edu>
2024-09-13 22:10:13 -0400

    initial layout

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+
+//this global variable contains an array of question objects
+//this data structure contain all the q and a info for the quiz
+var questionsArr = [
+    {
+        question: 'JavaScript is the best language',
+        answer: true
+    }
+]
+
+//the run quiz function contains the code for the quiz
+function runQuiz {
+    //for = keyword, (marks the condition/counter)
+    for (var i = 0; i < 10; i++) {
+        //this is the code to execute during loop
+        document.write(i);
+    }
+}
