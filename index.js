37b6a9508a25fa4ddcd8c392c0f42e503da750be
Cassie Andrews <candrews1@ufl.edu>
2024-09-14 13:07:51 -0400

    for loop initial set up

--- index.js
+++ index.js
@@ -2,18 +2,21 @@
 
 //this global variable contains an array of question objects
 //this data structure contain all the q and a info for the quiz
+function runQuiz() {
+    console.log('here')
+}
+
+
+//the run quiz function contains the code for the quiz
 var questionsArr = [
     {
-        question: 'JavaScript is the best language',
+        question: 'Question 1',
         answer: true
     }
 ]
 
-//the run quiz function contains the code for the quiz
-function runQuiz {
-    //for = keyword, (marks the condition/counter)
-    for (var i = 0; i < 10; i++) {
-        //this is the code to execute during loop
-        document.write(i);
-    }
+//for = keyword, (marks the condition/counter)
+for (let i = 0; i < questionsArr.length; i++) {
+    const answer = questionsArr[i];
+    
 }
