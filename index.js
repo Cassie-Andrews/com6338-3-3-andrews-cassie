0972bc513454cae2a2d98fc4fbdc46f85e356099
Cassie Andrews <candrews1@ufl.edu>
2024-09-14 22:56:44 -0400

    array updates

--- index.js
+++ index.js
@@ -4,17 +4,31 @@
 //this data structure contain all the q and a info for the quiz
 function runQuiz() {
     var questionsArr = [
-        {question: 'Question 1', answer: true},
-        {question: 'Question 2', answer: true},
-        {question: 'Question 3', answer: true},
-        {question: 'Question 4', answer: true},
-        {question: 'Question 5', answer: true}
-    ]
+        {
+            question:'Question 1',
+            answer:true
+        },
+        {
+            question:'Question 2',
+            answer:false 
+        },
+        {
+            question:'Question 3',
+            answer:true
+        },
+        {
+            question:'Question 4',
+            answer:false
+        },
+        {
+            question:'Question 5',
+            answer:true
+        }
+    ];
 
     //for = keyword, (marks the condition/counter)
     for (var i = 0; i < questionsArr.length; i++) {
         var question = questionsArr[i].question
-        console.log(question) //this worked properly but content in next step is off
-        var answer = confirm(question) //showing [object Object] in confirm
+        var answer = confirm(question)
     }
 }
